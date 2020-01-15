552eeacc802f20dc0bec14279fcfc30773a38a45
zileyuan <zileyuan@qq.com>
2020-01-16 00:26:51 +0800

    push 集成

--- ios/Classes/UmengAnalyticsPushPlugin.m
+++ ios/Classes/UmengAnalyticsPushPlugin.m
@@ -4,6 +4,13 @@
 #import <UMPush/UMessage.h>
 #import <UserNotifications/UserNotifications.h>
 
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+
+@interface UmengAnalyticsPushPlugin () <UNUserNotificationCenterDelegate>
+@end
+
+#endif
+
 @implementation UmengAnalyticsPushPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FlutterMethodChannel* channel = [FlutterMethodChannel
@@ -11,6 +18,7 @@
             binaryMessenger:[registrar messenger]];
   UmengAnalyticsPushPlugin* instance = [[UmengAnalyticsPushPlugin alloc] init];
   [registrar addMethodCallDelegate:instance channel:channel];
+  [registrar addApplicationDelegate:instance];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
@@ -59,6 +67,26 @@
   NSString *type = call.arguments[@"type"];
   [UMessage removeAlias:alias type:type response:^(id  _Nonnull responseObject, NSError * _Nonnull error) {
   }];
+}
+
+- (BOOL)application:(UIApplication *)application
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    [UMConfigure setLogEnabled:YES];
+
+    [UMConfigure initWithAppkey:@"你的友盟iOSAppKey" channel:@"appstore"];
+    UMessageRegisterEntity *entity = [[UMessageRegisterEntity alloc] init];
+    //type是对推送的几个参数的选择，可以选择一个或者多个。默认是三个全部打开，即：声音，弹窗，角标
+    entity.types = UMessageAuthorizationOptionBadge | UMessageAuthorizationOptionAlert | UMessageAuthorizationOptionSound;
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+    [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+#endif
+    [UMessage registerForRemoteNotificationsWithLaunchOptions:launchOptions Entity:entity completionHandler:^(BOOL granted, NSError *_Nullable error) {
+        if (granted) {
+        } else {
+        }
+    }];
+    return YES;
 }
 
 @end
